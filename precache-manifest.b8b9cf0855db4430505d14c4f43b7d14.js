4cc47ef0a4ddb2b55758e889bad3abcf697561c0
WWS <fauzanfalke@gmail.com>
2020-04-23 20:26:25 +0530

    Updates

---
+++ precache-manifest.b8b9cf0855db4430505d14c4f43b7d14.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47fb676d5ea404f27059eb2954687073",
+    "url": "/food-recipe-app/index.html"
+  },
+  {
+    "revision": "e7b0fcdb782ba6b7ba83",
+    "url": "/food-recipe-app/static/css/main.e7f0ffb1.chunk.css"
+  },
+  {
+    "revision": "af6d240f173108cdab81",
+    "url": "/food-recipe-app/static/js/2.4f7c347e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/food-recipe-app/static/js/2.4f7c347e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7b0fcdb782ba6b7ba83",
+    "url": "/food-recipe-app/static/js/main.913b4419.chunk.js"
+  },
+  {
+    "revision": "646cf2e3faa5980ba933",
+    "url": "/food-recipe-app/static/js/runtime-main.eba00d24.js"
+  },
+  {
+    "revision": "23396fc79f462052256ae283ebc2c674",
+    "url": "/food-recipe-app/static/media/logo.23396fc7.png"
+  }
+]);
